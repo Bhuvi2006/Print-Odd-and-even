1b2a85ad232574b3c6d14b504c00d04e236eb74f
Bhuvi21 <bhuvijai2006@gmail.com>
2024-08-20 08:26:46 +0530

    Add files via upload

---
+++ Odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Update the code below this line
+    int i = 1;
+    while (i < 20) {
+        printf("%d\n",i);
+        i+=2;
+
+    }
+    
+    return 0;
+}
